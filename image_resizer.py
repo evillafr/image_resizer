b4b6bb69dce98305874f2a9abcb48c52980848ee
Eliana Villafranca <villafranca.eliana@gmail.com>
2020-02-20 11:35:03 -0500

    get original width and height of the picture and decide which to use to resize

--- image_resizer.py
+++ image_resizer.py
@@ -2,3 +2,10 @@ import sys
 from PIL import Image
 
 file_name = sys.argv[1]
+picture = Image.open(file_name)
+width = picture.size[1]
+height = picture.size[0]
+if width >= height:
+	new_size = width
+else:
+	new_size = height

137b780030b9ab3d72ad9a10c89334d2aac16de5
Eliana Villafranca <villafranca.eliana@gmail.com>
2020-02-20 11:41:00 -0500

    creates a resized image and saves it with a name provided in the args

--- image_resizer.py
+++ image_resizer.py
@@ -2,6 +2,7 @@ import sys
 from PIL import Image
 
 file_name = sys.argv[1]
+new_name = sys.argv[2]
 picture = Image.open(file_name)
 width = picture.size[1]
 height = picture.size[0]
@@ -9,3 +10,5 @@ if width >= height:
 	new_size = width
 else:
 	new_size = height
+new_picture = picture.resize((new_size, new_size))
+new_picture.save(new_name)
